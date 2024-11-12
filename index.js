8870b87adb39419fde583322c1a15f78d022d813
Sebastian Betancurth <33584221+SebastianBetancurth@users.noreply.github.com>
2024-11-12 01:17:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors({
-  origin: ['tesis-ecom-production.up.railway.app', 'http://127.0.0.1:5501'], 
+ origin: ['http://localhost:8080', 'http://127.0.0.1:5501'], 
 }));
 
 app.use(morgan('dev'));

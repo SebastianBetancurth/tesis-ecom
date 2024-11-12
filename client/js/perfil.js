20f2291dba97d65c776c5b8abf189c702cd3ae8a
Sebastian Betancurth <33584221+SebastianBetancurth@users.noreply.github.com>
2024-11-11 22:45:51 -0300

    Update perfil.js

--- client/js/perfil.js
+++ client/js/perfil.js
@@ -2,7 +2,7 @@ window.onload = function() {
     const usuario = JSON.parse(localStorage.getItem('usuario'));
 
     if (usuario && usuario.id_cliente) {
-        fetch(`http://localhost:8080/usuario/verUsuario/${usuario.id_cliente}`)
+        fetch(`https://tesis-ecom.vercel.app/usuario/verUsuario/${usuario.id_cliente}`)
         .then(response => {
             if (!response.ok) {
                 throw new Error("Error al obtener los datos del usuario");

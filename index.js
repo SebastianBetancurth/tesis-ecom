4687b72cd1873ce199b8745311bfe35db2e99aed
Sebastian Betancurth <33584221+SebastianBetancurth@users.noreply.github.com>
2024-11-12 00:09:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,22 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.PORTDB,
-});
 
-
-db.connect((err) => {
-  if (err) {
-    console.error('Error al conectar a la base de datos:', err.stack);
-    return;
-  }
-  console.log('Conectado a la base de datos MySQL');
-});
 
 
 app.use(cors({

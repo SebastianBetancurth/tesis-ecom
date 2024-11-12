62cbbacc88ee05523796256eea27c7a130a1c5de
Sebastian Betancurth <33584221+SebastianBetancurth@users.noreply.github.com>
2024-11-12 00:40:53 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME|| 'ecommerce',
+  port: process.env.PORTDB || 8080,
 });
 module.exports=pool;
